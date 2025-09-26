bd1c920e73261466b336a0b21fc8fdccc745e59e
truptisatsangi <truptisatsangi18@gmail.com>
2025-09-27 00:52:06 +0530

    added demo.py

---
+++ demo.py
@@ -0,0 +1,11 @@
+from uagents import Agent, Context
+ 
+agent = Agent(name="alice", seed="secret_seed_phrase", port=8000, endpoint=["http://localhost:8000/submit"])
+ 
+@agent.on_event("startup")
+async def introduce_agent(ctx: Context):
+    ctx.logger.info(f"Hello, I'm agent {agent.name} and my address is {agent.address}.")
+ 
+if __name__ == "__main__":
+    agent.run()
+ 
